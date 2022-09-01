605e1536d0b12baec038b8d6624503560bbae625
Dmitriy Tikhonov <dstikhonov@rt-dc.ru>
2022-09-01 13:12:08 +0700

    Сделал 3ю задачу

--- CyclesTheme.java
+++ CyclesTheme.java
@@ -5,6 +5,7 @@ class CyclesTheme {
         int i = -10;
         int sumEvenNum = 0;
         int sumOddNumb = 0;
+
         do {
             if (i % 2 == 0) {
                 sumEvenNum += i;
@@ -17,6 +18,22 @@ class CyclesTheme {
         System.out.println("В промежутке [-10, 21] сумма четных чисел = " + sumEvenNum + ", а нечетных = " + sumOddNumb);
 
         System.out.println("\n2. Вывод чисел в интервале (min и max) в порядке убывания");
-        
+
+        for (i = 9; i > -1; i--) {
+            System.out.print(i + " "); // Уточнить у ментора как правильно решить
+        }
+
+        System.out.println("\n3. Вывод реверсивного числа и суммы его цифр");
+
+        int num = 1234;
+        int sum = 0;
+
+        while (num > 0) {
+            int resReverse = num % 10;
+            num /= 10;
+            sum += resReverse;
+            System.out.print(resReverse + " ");
+        }
+        System.out.println("\nСумма цифр = " + sum);
     }
 }
